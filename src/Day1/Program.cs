4bb5ecd606d7228d2dfc7244d7a98d90aa9cef9f
Julian Hüppauff <julian@hueppauff.com>
2021-12-01 22:41:50 +0100

    added day one

---
+++ src/Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] inputFile = await File.ReadAllLinesAsync(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+int[] measurements = (Array.ConvertAll(inputFile, x => Int32.Parse(x)));
+
+Console.WriteLine($"Solution of Task One: {SolveTaskOne(measurements)}");
+Console.WriteLine($"Solution of Task Two: {SolveTaskTwo(measurements)}");
+
+int SolveTaskOne(int[] measurements)
+{
+    int countOfMeasurementIncreases = 0;
+
+    for (int i = 1; i < measurements.Count(); i++)
+    {
+        if (measurements[i] > measurements[i - 1])
+        {
+            countOfMeasurementIncreases++;
+        }
+    }
+
+    return countOfMeasurementIncreases;
+}
+
+int SolveTaskTwo(int[] measurements)
+{
+    int countOfMeasurementIncreases = 0;
+
+    for (int i = 1; i < measurements.Count() - 2; i++)
+    {
+        int currentMeasurement = measurements[i - 1] + measurements[i] + measurements[i + 1];
+        int nextMeasurement = measurements[i] + measurements[i + 1] + measurements[i + 2];
+
+        if (currentMeasurement < nextMeasurement)
+        {
+            countOfMeasurementIncreases++;
+        }
+    }
+
+    return countOfMeasurementIncreases;
+}
